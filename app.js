6db2b82378c8db0b6a5be8d2928e7c71f9fb134f
Madelyn Romberg <madelynromberg@g.ucla.edu>
2023-02-28 17:02:20 -0800

    collect form data

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Let's get this party started!");
+"use strict";
+const $search = $("#search");
+let $searchVal;
+
+
+async function getGifGiphy(word = $searchVal){
+
+}
+
+
+
+
+
+
+
+
+
+/**getFormData: Gets search word and resets rearch input in DOM */
+function getFormData (){
+  $searchVal = $search.val();
+  $search.val("");
+  return $searchVal
+}
+
+/**
+ * handleClick: On form submit, add img to page
+ * */
+function handleClick(evt){
+  evt.preventDefault();
+  console.log('handleClick ran');
+
+  //append image
+
+}
