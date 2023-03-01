749bdd60cc62f863f633f9709638346580bfa8ac
Vaughn Seekamp <vseekamp@gmail.com>
2023-02-28 17:58:27 -0800

    cats were added to the page

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 "use strict";
 const $search = $("#search");
-let $searchVal;
+const giphyAddress = "http://api.giphy.com/v1/gifs/search";
+const giphyApiKey = "MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym";
 
-
-async function getGifGiphy(word = $searchVal){
-
+async function getGifGiphy(word){
+  const callGiphy = await axios.get(
+    `${giphyAddress}`,
+    { params: { q:word, api_key: giphyApiKey } }
+    );
+  console.log(callGiphy, "We called giphy with this");
+  const gifOne = callGiphy.data.data[0].images.original.url;
+  console.log(gifOne);
+  return gifOne;
 }
 
 
@@ -17,18 +24,24 @@ async function getGifGiphy(word = $searchVal){
 
 /**getFormData: Gets search word and resets rearch input in DOM */
 function getFormData (){
-  $searchVal = $search.val();
+  const $inputVal = $search.val();
   $search.val("");
-  return $searchVal
+  return $inputVal;
 }
 
 /**
  * handleClick: On form submit, add img to page
  * */
-function handleClick(evt){
+async function handleClick(evt){
   evt.preventDefault();
   console.log('handleClick ran');
+  const $searchVal = getFormData();
+  const image = await getGifGiphy($searchVal);
 
   //append image
+const $imageDiv = $(".img-party");
+$imageDiv.append(`<img src=${image}> </img>`);
+
 
 }
+$("form").on("submit", handleClick);
